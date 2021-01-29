181c42f5fdb83202813704c588d721f01c7866bb
Oleg <shpadio@gmail.com>
2021-01-29 12:23:24 +0300

    login

--- client/src/components/auth/Login/Login.js
+++ client/src/components/auth/Login/Login.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import {React, useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import {useDispatch} from "react-redux";
+import {authSuccessAC} from "../../../redux/actionCreators/authAC";
 
 function Login(props) {
+    const dispatch = useDispatch()
+    const history = useHistory()
+
+    const [error, setError] = useState()
+    const [email, setEmail] = useState()
+    const [password,setPassword] = useState()
+
+    const emailHandler = (event) => {
+        const email = event.target.email.value
+        setEmail(email)
+    }
+
+    const passwordHandler = (event) => {
+        const password = event.target.password.value
+        setPassword(password)
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        fetch(`${process.env.REACT_APP_URL}/api/auth/login`,{
+            method:'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({email,password})
+        }).then(response => response.json())
+            .then((serverData) => {
+                if(serverData.user){
+                    dispatch(authSuccessAC(serverData.user));
+                    return history.push('/dashboard')
+                }
+                return setError('Wrong email or password');
+            }).catch(() => setError('Wrong email or password'))
+    };
+
+
+
+
     return (
-        <div>
-            Login
-        </div>
+        <form onSubmit={submitHandler}>
+            <input onChange={emailHandler} type="text"  placeholder="Enter your email" value={email}/>
+            <input onChange={passwordHandler} type="password" name="password" placeholder='Enter your password' value={password}/>
+            <button type="submit">Login</button>
+            <div>{error && error}</div>
+        </form>
     );
 }
 
---
+++ client/src/redux/reducers/authReducer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {AUTH_SUCCESSFULLY,LOGOUT} from '../types'
+
+const windowState = JSON.parse((window.localStorage.getItem('state')))
+
+let preloadedState = {};
+
+if(windowState && windowState.auth){
+    preloadedState = {isAuth : windowState.auth.isAuth,user: windowState.auth.user};
+} else {
+    preloadedState = { isAuth : false, user : {email:'',username:''} };
+}
+
+const authReducer = (state = preloadedState, action) => {
+    switch (action.type) {
+        case AUTH_SUCCESSFULLY:
+            return {
+                ...state,
+                isAuth: true,
+                user: action.payload
+            };
+        case LOGOUT:
+            window.localStorage.removeItem('state')
+            return {
+                ...state,
+                isAuth: false,
+                user : {email:'',username:''}
+            };
+        default:
+            return state;
+
+    }
+}
+
+export default authReducer;
